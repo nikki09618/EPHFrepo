29241b9dc377fbeea8242311243da11f2189fd82
nikki09618 <nikkipatel09618@gmail.com>
2025-11-14 08:00:30 +0530

    Add login function

--- Day_5_Github/Q_12/index.js
+++ Day_5_Github/Q_12/index.js
@@ -8,3 +8,16 @@ function signup(userName) {
         return 'Signup Sucessfull, Please Login';
     }
 }
+function login(userName, password) {
+    const usernames = ['Nikita_patel', 'Kajal_solanki', 'Arohi_patel', 'Princi_mehra'];
+    
+    if (usernames.includes(userName)) {
+        if (password === 'Emp@123') {
+            return 'Login Sucessfull...';
+        } else {
+            return 'Wrong Password....';
+        }
+    } else {
+        return 'User Not Found, Please Signup';
+    }
+}
